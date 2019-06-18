b4d5f4ad330361c57170dff651b55066ad99ddbc
Dalila <dalila.herh@gmail.com>
2019-06-18 15:02:45 +0200

    tutorijal 9 završen

---
+++ src/ba/unsa/etf/rs/tutorijal8/Controller.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rs.tutorijal8;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void dodajBus(ActionEvent actionEvent) {
+    }
+
+    public void obrisiVozaca(ActionEvent actionEvent) {
+    }
+
+    public void obrisiBus(ActionEvent actionEvent) {
+    }
+
+    public void akcijaEditDrivera(ActionEvent actionEvent) {
+    }
+
+    public void akcijaEditBus(ActionEvent actionEvent) {
+    }
+
+    public void dodajVozaca(ActionEvent actionEvent) {
+    }
+}
---
+++ src/ba/unsa/etf/rs/tutorijal8/DriverController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rs.tutorijal8;
+
+import javafx.event.ActionEvent;
+
+public class DriverController {
+    public void akcijaCancel(ActionEvent actionEvent) {
+    }
+
+    public void akcijaOk(ActionEvent actionEvent) {
+    }
+}
